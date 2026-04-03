ff154cd6a7df8f6140a04fb6ee70667e1762206a
Ariel Ivan Arellano <aarellano@frba.utn.edu.ar>
2026-04-03 18:50:16 +0000

    cerrar conexion al terminar el cliente

--- client/src/client.c
+++ client/src/client.c
@@ -107,8 +107,10 @@ void paquete(int conexion)
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
-	log_destroy(logger);
-	config_destroy(config);
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  
+	log_destroy(logger);
+	config_destroy(config);
+	liberar_conexion(conexion);
 }
