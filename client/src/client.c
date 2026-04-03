e8859804a044a694162d4fe66965d3a4a0ccc7ad
Ariel Ivan Arellano <aarellano@frba.utn.edu.ar>
2026-04-03 15:58:15 +0000

    agregar logger basico

--- client/src/client.c
+++ client/src/client.c
@@ -16,6 +16,8 @@ int main(void)
 
 	logger = iniciar_logger();
 
+	log_info(logger, "Hola! Soy un log");
+
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
 
@@ -54,7 +56,7 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0.log", "Cliente", 1, LOG_LEVEL_INFO);
 
 	return nuevo_logger;
 }
@@ -95,6 +97,7 @@ void paquete(int conexion)
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
+	log_destroy(logger);
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 }
